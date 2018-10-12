02590bd978faffec8f3ae276bb47dacc750e21df
Parthy Chandrasekaran <parthy_chandrasekaran@trendmicro.com>
2018-10-12 09:20:00 -0400

    script to compile and run sample

--- runme.sh
+++ runme.sh
@@ -2,3 +2,4 @@
 # https://www.bouncycastle.org/download/bctls-jdk15on-160.jar
 # https://www.bouncycastle.org/download/bcprov-ext-jdk15on-160.jar
 javac -cp bcprov-ext-jdk15on-160.jar:bctls-jdk15on-160.jar:. TlsExample1.java
+java -cp bcprov-ext-jdk15on-160.jar:bctls-jdk15on-160.jar:. TlsExample1
